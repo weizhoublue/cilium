974118ad78dc5f275c273e074241dfb9c7fe7c3c
Thomas Graf <thomas@cilium.io>
2019-05-10 16:14:00 +0200

    ipam: Provide ipam information in debuginfo
    Signed-off-by: Thomas Graf <thomas@cilium.io>

--- pkg/ipam/types.go
+++ pkg/ipam/types.go
@@ -19,6 +19,8 @@ import (
 
 	"github.com/cilium/cilium/pkg/datapath"
 	"github.com/cilium/cilium/pkg/lock"
+
+	"github.com/davecgh/go-spew/spew"
 )
 
 // Allocator is the interface for an IP allocator implement
@@ -51,4 +53,13 @@ type IPAM struct {
 	allocatorMutex lock.RWMutex
 
 	blacklist map[string]string
+}
+
+// DebugStatus implements debug.StatusObject to provide debug status collection
+// ability
+func (ipam *IPAM) DebugStatus() string {
+	ipam.allocatorMutex.RLock()
+	str := spew.Sdump(ipam)
+	ipam.allocatorMutex.RUnlock()
+	return str
 }
