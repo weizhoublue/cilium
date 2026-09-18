577d5a4edbf55e38e460acce818e6300881a6bbe
weizhoublue <weizhou.lan@daocloud.io>
2026-09-18 03:43:15 -0700

    Gateway TLS sync fails for Secret names exceeding 63 characters
    Signed-off-by: weizhoublue <weizhou.lan@daocloud.io>

--- operator/pkg/secretsync/ownership.go
+++ operator/pkg/secretsync/ownership.go
@@ -35,20 +35,31 @@ func ownerFromLabels(obj client.Object, namespaceLabel, nameLabel string) (types
 	return types.NamespacedName{Namespace: namespace, Name: name}, true
 }
 
+func ownerFromAnnotationsOrLabels(obj client.Object, namespaceLabel, nameLabel string) (types.NamespacedName, bool) {
+	annotations := obj.GetAnnotations()
+	namespace, hasNamespace := annotations[SourceNamespaceAnnotation]
+	name, hasName := annotations[SourceNameAnnotation]
+	if hasNamespace && hasName && namespace != "" && name != "" {
+		return types.NamespacedName{Namespace: namespace, Name: name}, true
+	}
+
+	return ownerFromLabels(obj, namespaceLabel, nameLabel)
+}
+
 func isOwnedBy(obj client.Object, owner types.NamespacedName, namespaceLabel, nameLabel string) bool {
-	existingOwner, ok := ownerFromLabels(obj, namespaceLabel, nameLabel)
+	existingOwner, ok := ownerFromAnnotationsOrLabels(obj, namespaceLabel, nameLabel)
 	return ok && existingOwner == owner
 }
 
 func ensureOwnedBy(existing, desired client.Object, namespaceLabel, nameLabel string) error {
-	desiredOwner, ok := ownerFromLabels(desired, namespaceLabel, nameLabel)
+	desiredOwner, ok := ownerFromAnnotationsOrLabels(desired, namespaceLabel, nameLabel)
 	if !ok {
-		return fmt.Errorf("desired synced Secret %s/%s is missing ownership labels", desired.GetNamespace(), desired.GetName())
+		return fmt.Errorf("desired synced Secret %s/%s is missing ownership metadata", desired.GetNamespace(), desired.GetName())
 	}
 
-	existingOwner, ok := ownerFromLabels(existing, namespaceLabel, nameLabel)
+	existingOwner, ok := ownerFromAnnotationsOrLabels(existing, namespaceLabel, nameLabel)
 	if !ok {
-		return fmt.Errorf("refusing to overwrite synced Secret %s/%s without ownership labels", existing.GetNamespace(), existing.GetName())
+		return fmt.Errorf("refusing to overwrite synced Secret %s/%s without ownership metadata", existing.GetNamespace(), existing.GetName())
 	}
 	if existingOwner != desiredOwner {
 		return fmt.Errorf("refusing to overwrite synced Secret %s/%s owned by %s/%s with data from %s/%s", existing.GetNamespace(), existing.GetName(), existingOwner.Namespace, existingOwner.Name, desiredOwner.Namespace, desiredOwner.Name)
